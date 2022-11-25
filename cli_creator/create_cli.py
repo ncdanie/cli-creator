1af5f977b5ed6f1c9560bf3970cc282cc94f4bc9
ncdanie <nathancdaniels@gmail.com>
2022-11-25 15:39:22 -0800

    initial commit

---
+++ cli_creator/create_cli.py
@@ -0,0 +1,6 @@
+'''Define create_cli decorator.'''
+import argparse
+
+def create_cli(decorated):
+    '''Decorator that creates a command line interface for functions and class methods'''
+    pass
